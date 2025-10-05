890d40782d63eb5d0339ea224adaae95015102b4
joolean-equals-true <160288072+joolean-equals-true@users.noreply.github.com>
2025-10-05 18:24:28 -0500

    fixed app folder

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ var logger = require('morgan');
 
 
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./app/routes/index');
+var usersRouter = require('./app/routes/users');
 
-var db = require('./controllers/dboperations')
-var course = require('./models/courses')
+var db = require('./app/controllers/dboperations')
+var course = require('./app/models/courses')
 var body_parser = require('body-parser')
 var cors = require('cors')
 var app = express();
